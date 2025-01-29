faf07a813a3662f18df20c8377bd9bfd6a6b1856
wacho91 <criskol.71@gmail.com>
2025-01-29 11:35:39 -0500

    Navbar Component complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,37 +12,40 @@ const Navbar = () => {
     }
 
   return (
-    <nav>
-        <div>
-            <div>
-                <div>
-                    <img src={Logo} alt="logo" />
-                    <span>VirtualR</span>
+    <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
+        <div className="container px-4 mx-auto relative lg:text-sm">
+            <div className="flex justify-between items-center">
+                <div className="flex items-center flex-shrink-0">
+                    <img className="h-10 w-10 mr-2" src={Logo} alt="logo" />
+                    <span className="text-xl tracking-tight">VirtualR</span>
                 </div>
-                <ul>
+                <ul className="hidden lg:flex ml-14 space-x-12">
                     {navItems.map((item, index) => (
                         <li key={index}>
                             <a href={item.href}>{item.label}</a>
                         </li>
                     ))}
                 </ul>
-                <div>
-                    <a href="#">
+                <div className="hidden lg:flex justify-center space-x-12 items-center">
+                    <a href="#" className="py-2 px-3 border rounded-md">
                         Sign In
                     </a>
 
-                    <a href="#">
+                    <a 
+                        href="#"
+                        className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md"
+                    >
                         Create an account
                     </a>
                 </div>
-                <div>
+                <div className="lg:hidden md:flex flex-col justify-end">
                     <button onClick={toggleNavbar}>
                         {mobileDrawerOpen ? <X /> : <Menu />}
                     </button>
                 </div>
             </div>
             {mobileDrawerOpen && (
-                <div>
+                <div className="fixed right-0 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden">
                     <ul>
                         {navItems.map((item, index) => (
                             <li key={index}>
@@ -50,11 +53,14 @@ const Navbar = () => {
                             </li>
                         ))}
                     </ul>
-                    <div>
-                        <a href="#">
+                    <div className="flex space-x-6">
+                        <a href="#" className="py-2 px-3 border rounded-md">
                             Sign In
                         </a>
-                        <a href="#">
+                        <a 
+                            href="#"
+                            className="py-2 px-3 rounded-md bg-gradient-to-r from-orange-500 to-orange-800"
+                        >
                             Create an account
                         </a>
                     </div>
