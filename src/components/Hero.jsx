7dda5a3507a66465d6e22085d9dbe949ce12b4df
wacho91 <criskol.71@gmail.com>
2025-01-29 11:49:36 -0500

    Arquitecture component complete

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,23 @@ const Hero = () => {
             into immersive reality!
         </p>
         <div>
-            
+            <a href="#">
+                Start for free
+            </a>
+            <a href="#">
+                Documentation
+            </a>
+        </div>
+        <div>
+            <video>
+                <source src={video1} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+
+            <video>
+                <source src={video2} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
         </div>
     </div>
   )
