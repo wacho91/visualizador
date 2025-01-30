e66d46dbc5356c80434df033ec4e94ec6384dd5f
wacho91 <criskol.71@gmail.com>
2025-01-30 11:34:04 -0500

    Arquitecture component testimonial

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -3,7 +3,30 @@ import { testimonial } from "../constants"
 const Testimonial = () => {
   return (
     <div>
-        Testimonial component
+        <h2>
+            What People are saying
+        </h2>
+        <div>
+            {testimonial.map((testimonial, index) => (
+                <div key={index}>
+                    <div>
+                        <p>{testimonial.text}</p>
+                        <div>
+                            <img 
+                                src={testimonial.image} 
+                                alt="image" 
+                            />
+                            <div>
+                                <h6>{testimonial.user}</h6>
+                                <span>
+                                    {testimonial.company}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
     </div>
   )
 }
