7958de44084d12258dcc3115959f1e2fe97ab653
wacho91 <criskol.71@gmail.com>
2025-01-30 11:43:57 -0500

    Testimonial component complete

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -3,22 +3,23 @@ import { testimonial } from "../constants"
 const Testimonial = () => {
   return (
     <div className="mt-20 tracking-wide">
-        <h2>
+        <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center my-10 lg:my-20">
             What People are saying
         </h2>
-        <div>
+        <div className="flex flex-wrap justify-center">
             {testimonial.map((testimonial, index) => (
-                <div key={index}>
-                    <div>
+                <div key={index} className="w-full sm:w-1/2 lg:w-1/3 px-4 py-2">
+                    <div className="bg-neutral-900 rounded-md p-6 text-md border border-neutral-800 font-thin">
                         <p>{testimonial.text}</p>
-                        <div>
+                        <div className="flex mt-8 items-start">
                             <img 
                                 src={testimonial.image} 
+                                className="w-12 h-12 mr-6 rounded-full border border-neutral-300"
                                 alt="image" 
                             />
                             <div>
                                 <h6>{testimonial.user}</h6>
-                                <span>
+                                <span className="text-sm font-normal italic text-neutral-600">
                                     {testimonial.company}
                                 </span>
                             </div>
