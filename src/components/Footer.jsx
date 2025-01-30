a2ba6eec63d6b228cbd012cdbfeff874c25ccc9f
wacho91 <criskol.71@gmail.com>
2025-01-30 12:00:44 -0500

    Arquitecture component  footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,53 @@
-
+import { communityLinks, platformLinks, resourcesLinks } from "../constants"
 
 const Footer = () => {
   return (
-    <div>
-        Footer Component
-    </div>
+    <footer>
+        <div>
+            <div>
+                <h3>Resources</h3>
+                <ul>
+                    {resourcesLinks.map((Link, index) => (
+                        <li key={index}>
+                            <a 
+                                href={Link.href}
+                            >
+                                {Link.text}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div>
+                <h3>Platform</h3>
+                <ul>
+                    {platformLinks.map((link, index) => (
+                        <li key={index}>
+                            <a 
+                                href={link.href}
+                            >
+                                {link.text}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div>
+                <h3>Community</h3>
+                <ul>
+                    {communityLinks.map((link, index) => (
+                        <li key={index}>
+                            <a 
+                                href={link.href}
+                            >
+                                {link.text}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    </footer>
   )
 }
 
