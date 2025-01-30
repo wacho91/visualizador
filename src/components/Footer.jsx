9d8965de8d6e88da3964cf28322faf5214300e58
wacho91 <criskol.71@gmail.com>
2025-01-30 12:07:33 -0500

    Footer component complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,15 +2,16 @@ import { communityLinks, platformLinks, resourcesLinks } from "../constants"
 
 const Footer = () => {
   return (
-    <footer>
-        <div>
+    <footer className="mt-20 border-t py-10 border-neutral-700">
+        <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
             <div>
-                <h3>Resources</h3>
-                <ul>
+                <h3 className="text-md font-semibold mb-4">Resources</h3>
+                <ul className="space-y-2">
                     {resourcesLinks.map((Link, index) => (
                         <li key={index}>
                             <a 
                                 href={Link.href}
+                                className="text-neutral-300 hover:text-white"
                             >
                                 {Link.text}
                             </a>
@@ -19,12 +20,13 @@ const Footer = () => {
                 </ul>
             </div>
             <div>
-                <h3>Platform</h3>
-                <ul>
+                <h3 className="text-md font-semibold mb-4">Platform</h3>
+                <ul className="space-y-2">
                     {platformLinks.map((link, index) => (
                         <li key={index}>
                             <a 
                                 href={link.href}
+                                className="text-neutral-300 hover:text-white"
                             >
                                 {link.text}
                             </a>
@@ -33,14 +35,16 @@ const Footer = () => {
                 </ul>
             </div>
             <div>
-                <h3>Community</h3>
-                <ul>
+                <h3 className="text-md font-semibold mb-4">Community</h3>
+                <ul className="space-y-2">
                     {communityLinks.map((link, index) => (
                         <li key={index}>
                             <a 
                                 href={link.href}
+                                className="text-neutral-300 hover:text-white"
                             >
                                 {link.text}
+                                
                             </a>
                         </li>
                     ))}
