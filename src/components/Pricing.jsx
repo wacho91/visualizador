5bd1218005ed6b7d329d777a39e97913f2253f3f
wacho91 <criskol.71@gmail.com>
2025-01-30 11:19:19 -0500

    Pricing component Complete

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -3,35 +3,38 @@ import { pricingOptions } from "../constants"
 
 const Pricing = () => {
   return (
-    <div>
-        <h2>
+    <div className="mt-20">
+        <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center my-8 tracking-wide">
             Pricing
         </h2>
-        <div>
+        <div className="flex flex-wrap">
             {pricingOptions.map((option, index) => (
-                <div key={index}>
-                    <div>
-                        <p>
+                <div key={index} className="w-full sm:w-1/2 lg:w-1/3 p-2">
+                    <div className="p-10 border border-neutral-700 rounded-xl">
+                        <p  className="text-4xl mb-8">
                             {option.title}
                             {option.title === "Pro" && (
-                                <span>
+                                <span className="bg-gradient-to-r from-orange-500 to-red-400 text-transparent bg-clip-text text-xl mb-4 ml-2">
                                     (Most Popular)
                                 </span>
                             )}
                         </p>
-                        <p>
-                            <span>{option.price}</span>
-                            <span>Month</span>
+                        <p className="mb-8">
+                            <span className="text-5xl mt-6 mr-2">{option.price}</span>
+                            <span className="text-neutral-400 tracking-tight">Month</span>
                         </p>
                         <ul>
                             {option.features.map((feature, index) => (
-                                <li key={index}>
+                                <li key={index} className="mt-8 flex items-center">
                                     <CheckCircle2 />
-                                    <span>{feature}</span>
+                                    <span className="ml-2">{feature}</span>
                                 </li>
                             ))}
                         </ul>
-                        <a href="#">
+                        <a 
+                            href="#"
+                            className="inline-flex justify-center items-center text-center w-full h-12 p-5 mt-20 tracking-tight text-xl hover:bg-orange-900 border border-orange-900 rounded-lg transition duration-200"    
+                        >
                             Subscribe
                         </a>
                     </div>
